13c8eaf3216daedefc820a6149192716f19262ee
Md. Sajjad Hossain <ashrafazimsajjad@gmail.com>
2026-04-23 15:45:15 +0600

    init

---
+++ assignment1/variables/variables-1.py
@@ -0,0 +1,10 @@
+# Assign multiple variables in a single line
+a, b, c = 10, 20, 30
+
+# Print the values of the variables
+print("Value of a:", a)
+print("Value of b:", b)
+print("Value of c:", c)
+
+# You can also print all values in one line
+print("Values are:", a, b, c)
---
+++ assignment1/variables/variables-2.py
@@ -0,0 +1,11 @@
+# Store the value of pi
+pi = 3.1416
+
+# Define the radius
+radius = 5
+
+# Calculate the area of the circle using formula: area = pi * r^2
+area = pi * radius ** 2
+
+# Print the result
+print("Area of the circle is:", area)
